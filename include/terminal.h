17896aa7fc2dbee216eb209885d8fec1e35cdd70
Danny VR <38941247+dannybuilds@users.noreply.github.com>
2023-05-25 16:37:52 -0700

    terminal menu module started

---
+++ include/terminal.h
@@ -0,0 +1,13 @@
+#ifndef TERMINAL_H
+#define TERMINAL_H
+
+// Initialize the terminal
+void terminal_init(void);
+
+// Display the main menu
+void terminal_display_menu(void);
+
+// Handle user input
+void terminal_handle_input(void);
+
+#endif // TERMINAL_H
