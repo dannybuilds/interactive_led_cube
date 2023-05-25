363b91560fcddaa9c0f8c9d99924b7dee17efab9
Danny VR <38941247+dannybuilds@users.noreply.github.com>
2023-05-25 16:40:40 -0700

    program entry started

---
+++ include/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+// Start the main program loop
+void main_loop(void);
+
+#endif // MAIN_H
---
+++ src/main.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include "led_cube_controller.h"
+#include "sensor_manager.h"
+#include "object_detection.h"
+#include "terminal.h"
+#include "diagnostic.h"
+#include <stdio.h>
+
+// Start the main program loop
+void main_loop(void) {
+    // Initialize all modules
+    led_cube_init();
+    sensor_manager_init();
+    object_detection_init();
+    terminal_init();
+    diagnostic_run_tests();
+
+    // Display the main menu
+    terminal_display_menu();
+
+    // Enter the main program loop
+    while (1) {
+        // Handle user input
+        terminal_handle_input();
+
+        // Update the LED cube
+        led_cube_update();
+
+        // Display the current system status
+        diagnostic_display_status();
+    }
+}
+
+int main(void) {
+    // Start the main program loop
+    main_loop();
+
+    return 0;
+}
